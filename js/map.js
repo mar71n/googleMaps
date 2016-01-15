571edd6596081d07f24fbe22aaaa04cbe7555f47
mar71n <mar71n@gmail.com>
2016-01-15 13:13:10 -0300

    primer mapa

---
+++ js/map.js
@@ -0,0 +1,12 @@
+﻿var mapDiv = document.getElementById('map');
+var catalunya = new google.maps.LatLng(41.652393,1.691895);
+window.onload = function() {
+var mapDiv = document.getElementById('map');
+var catalunya = new google.maps.LatLng(41.652393,1.691895);
+var options = {
+center: catalunya,
+zoom: 8,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var mapa = new google.maps.Map(mapDiv, options);
+}

49aa281ca5cd53c761a3ba19f29e28636705431b
mar71n <mar71n@gmail.com>
2016-01-17 17:53:32 -0300

    disableDefaultUI

--- js/map.js
+++ js/map.js
@@ -4,7 +4,9 @@ var catalunya = new google.maps.LatLng(41.652393,1.691895);
 var options = {
 center: catalunya,
 zoom: 8,
-mapTypeId: google.maps.MapTypeId.ROADMAP
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+//el valor true desactiva la interfaz, el valor false (por defecto) la activa
+disableDefaultUI: true
 };
 var mapa = new google.maps.Map(mapDiv, options);
 }
